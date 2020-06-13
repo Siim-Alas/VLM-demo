2c84afff5db789027c63d8ec36619c380eea77c0
Siim Alas <siim.alas@outlook.com>
2020-06-13 16:51:58 +0300

    Added SimulationParametersModel along with form styling and wind vector display

---
+++ VortexLatticeBlazorUI/Models/SimulationParameterModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VortexLatticeBlazorUI.Models
+{
+    public class SimulationParameterModel
+    {
+        [Required]
+        public string File { get; set; } = "";
+        [Required]
+        public double WingSpan { get; set; } = 7;
+        [Required]
+        public double Chord { get; set; } = 1;
+        [Required]
+        public int NumberOfTilesSpanwise { get; set; } = 10;
+        [Required]
+        public int NumberOfTilesChordwise { get; set; } = 20;
+        [Required]
+        public double Rho { get; set; } = 1.225;
+        [Required]
+        public double MagnitudeOfVInfinity { get; set; } = 30;
+        [Required]
+        public double AOA { get; set; } = 0;
+        [Required]
+        public double AOY { get; set; } = 0;
+    }
+}
